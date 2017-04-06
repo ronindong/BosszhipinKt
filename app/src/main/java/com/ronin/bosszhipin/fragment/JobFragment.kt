b3df45e277c3185b6c467bca895a1b53f8b78acb
donghl <465430474@qq.com>
2017-04-06 18:32:02 +0800

    添加通用颜色xml文件color_comm

--- app/src/main/java/com/ronin/bosszhipin/fragment/JobFragment.kt
+++ app/src/main/java/com/ronin/bosszhipin/fragment/JobFragment.kt
@@ -1,40 +1,43 @@
 package com.ronin.bosszhipin.fragment
 
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.app.AppCompatActivity
+import android.view.*
 import com.ronin.bosszhipin.R
 import com.ronin.bosszhipin.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_job.view.*
 
 /**
  * Created by Administrator on 2017/4/1.
  */
 class JobFragment : BaseFragment() {
 
-     var toolBar:Toolbar? = null
+    var rootView: View? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        val rootView = inflater?.inflate(R.layout.fragment_job, container,false)
+        rootView = inflater?.inflate(R.layout.fragment_job, container, false)
 
-        toolBar = rootView?.findViewById(R.id.toolbar) as? Toolbar
-
-        toolBar?.title = "Title"
-        toolBar?.setNavigationIcon(R.drawable.ic_search_black_24dp)
-
-//        UI {
-//            coordinatorLayout {
-//                fitsSystemWindows = true
-//
-//            }
-//        }
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).setSupportActionBar(rootView!!.tool_bar)
+        rootView!!.tool_bar.title = "android"
 
 
         return rootView
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu!!.clear()
+        inflater!!.inflate(R.menu.navigation, menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        return super.onOptionsItemSelected(item)
+    }
 
 }
